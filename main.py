79f380ea66b6d36578f98e7ab00b63936e036083
Max <mrholdmb@gmail.com>
2023-04-09 22:37:53 +0300

    Preparing to deploy, added checking port in venv

--- main.py
+++ main.py
@@ -117,4 +117,5 @@ def about():
 
 if __name__ == '__main__':
     db_session.global_init("db/users.db")
-    app.run(port=8080, host='127.0.0.1', debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(port=port, host='0.0.0.0', debug=False)
